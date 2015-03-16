00880a9b07565315877943c85c0ec3fa643232c7
Joshua Nuez <frozen.nuez@gmail.com>
2015-03-15 19:36:03 -0700

    Some sample code to connect to database

---
+++ Sample/Sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+
+    [Table]
+
+    class _User
+    {
+        [Column(Name = "username")]
+        public string username;
+        [Column(Name = "password")]
+        public string password;
+        [Column(Name = "firstname")]
+        public string firstname;
+        [Column(Name = "lastname")]
+        public string lastname;
+        [Column(Name = "email")]
+        public string email;
+        [Column(Name = "address")]
+        public string address;
+        [Column(Name = "phonenumber")]
+        public string phonenumber;
+        [Column(Name = "verified")]
+        public bool verified;
+        [Column(Name = "admin")]
+        public bool admin;
+        [Column(Name = "ownedTeams")]
+        public string ownedTeams;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
